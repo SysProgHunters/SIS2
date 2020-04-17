cdcd451ce04ca368aab46a5808d5390a6e0489d2
BzzItsMe <bzzitsme@gmail.com>
2020-04-17 22:44:36 +0600

    Init

--- cpumonitor.c
+++ cpumonitor.c
@@ -24,7 +24,7 @@
 #include <sys/sysctl.h>         
 
 /******************************************************************
- * Utilities
+ * Utillities
  */
 
 #define MIN(a, b) ({                            \
